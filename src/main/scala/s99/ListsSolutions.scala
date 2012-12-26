f75808064cf6d2ad617f98f4c2e74315295febfa
Rui Gonçalves <ruippeixotog@gmail.com>
2012-12-26 23:03:13 +0000

    Solved problems P01 to P15.

--- src/main/scala/s99/ListsSolutions.scala
+++ src/main/scala/s99/ListsSolutions.scala
@@ -4,36 +4,149 @@ import Solutions._
 
 trait ListsSolutions {
 
-  def last[T](list: List[T]): T = ???
-  def penultimate[T](list: List[T]): T = ???
-  def nth[T](n: Int, list: List[T]): T = ???
-  def length[T](list: List[T]): Int = ???
-  def reverse[T](list: List[T]): List[T] = ???
-  def isPalindrome[T](list: List[T]): Boolean = ???
-  def flatten(list: List[Any]): List[Any] = ???
-  def compress[T](list: List[T]): List[T] = ???
-  def pack[T](list: List[T]): List[List[T]] = ???
-  def encode[T](list: List[T]): List[(Int, T)] = ???
-  def encodeModified[T](list: List[T]): List[Any] = ???
-  def decode[T](list: List[(Int, T)]): List[T] = ???
-  def encodeDirect[T](list: List[T]): List[(Int, T)] = ???
-  def duplicate[T](list: List[T]): List[T] = ???
-  def duplicateN[T](n: Int, list: List[T]): List[T] = ???
-  def drop[T](n: Int, list: List[T]): List[T] = ???
-  def split[T](n: Int, list: List[T]): (List[T], List[T]) = ???
-  def slice[T](i: Int, j: Int, list: List[T]): List[T] = ???
-  def rotate[T](n: Int, list: List[T]): List[T] = ???
-  def removeAt[T](i: Int, list: List[T]): (List[T], T) = ???
-  def insertAt[T](t: T, i: Int, list: List[T]): List[T] = ???
-  def range[T](i: Int, j: Int): List[Int] = ???
-  def randomSelect[T](n: Int, list: List[T]): List[T] = ???
-  def lotto[T](i: Int, j: Int): List[Int] = ???
-  def randomPermute[T](list: List[T]): List[T] = ???
-  def combinations[T](n: Int, list: List[T]): List[List[T]] = ???
-  def group3[T](list: List[T]): List[List[List[T]]] = ???
-  def groups[T](ns: List[Int], list: List[T]): List[List[List[T]]] = ???
-  def lsort[T](list: List[List[T]]): List[List[T]] = ???
-  def lsortFreq[T](list: List[List[T]]): List[List[T]] = ???
+  def last[T](list: List[T]): T = list match {
+    case x :: Nil => x
+    case _ :: xs => last(xs)
+    case Nil => throw new NoSuchElementException
+  }
 
+  def penultimate[T](list: List[T]): T = list match {
+    case x :: _ :: Nil => x
+    case _ :: xs => penultimate(xs)
+    case Nil => throw new NoSuchElementException
+  }
+
+  def nth[T](n: Int, list: List[T]): T =
+    if (n == 0) list.head
+    else nth(n - 1, list.tail)
+
+  def length[T](list: List[T]): Int = list match {
+    case Nil => 0
+    case x :: xs => length(xs) + 1
+  }
+
+  //  // tailrec version
+  //  def length[T](list: List[T]): Int = {
+  //    def lengthAux(list: List[T], acc: Int): Int = list match {
+  //      case Nil => acc
+  //      case x :: xs => lengthAux(xs, acc + 1)
+  //    }
+  //    lengthAux(list, 0)
+  //  }
+
+  def reverse[T](list: List[T]): List[T] = {
+    def reverseAux(list: List[T], acc: List[T]): List[T] = list match {
+      case Nil => acc
+      case x :: xs => reverseAux(xs, x :: acc)
+    }
+    reverseAux(list, Nil)
+  }
+
+  def isPalindrome[T](list: List[T]): Boolean = list == reverse(list)
+
+  def flatten(list: List[Any]): List[Any] = {
+    def flattenAux(elem: Any): List[Any] = elem match {
+      case Nil => Nil
+      case x :: xs => flattenAux(x) ::: flattenAux(xs)
+      case x => List(x)
+    }
+    flattenAux(list)
+  }
+
+  def compress[T](list: List[T]): List[T] = list match {
+    case Nil => Nil
+    case x :: x1 :: xs if x == x1 => compress(x1 :: xs)
+    case x :: xs => x :: compress(xs)
+  }
+
+  def pack[T](list: List[T]): List[List[T]] = {
+    def packAux(list: List[T], curr: List[T]): List[List[T]] = (list, curr) match {
+      case (Nil, Nil) => Nil
+      case (Nil, cs) => cs :: Nil
+      case (x :: xs, Nil) => packAux(xs, List(x))
+      case (x :: xs, c :: cs) =>
+        if (x == c) packAux(xs, x :: c :: cs)
+        else curr :: packAux(xs, List(x))
+    }
+    packAux(list, Nil)
+  }
+
+  def encode[T](list: List[T]): List[(Int, T)] =
+    pack(list).map { x => (length(x), x.head) }
+
+//  // without using map (has errors)
+//  def encode[T](list: List[T]): List[(Int, T)] = pack(list) match {
+//    case Nil => Nil
+//    case x :: xs => (length(x), x.head) :: encode(xs)
+//  }
+
+  def encodeModified[T](list: List[T]): List[Any] = encode(list).collect {
+    case (1, x) => x
+    case x => x
+  }
+
+  def decode[T](list: List[(Int, T)]): List[T] = list match {
+    case Nil => Nil
+    case (1, x) :: xs => x :: decode(xs)
+    case (n, x) :: xs => x :: decode((n - 1, x) :: xs)
+  }
+
+  def encodeDirect[T](list: List[T]): List[(Int, T)] = {
+    def encodeDirAux(list: List[T], curr: (Int, T)): List[(Int, T)] = list match {
+      case Nil => List(curr)
+      case x :: xs =>
+        if (x == curr._2) encodeDirAux(xs, (curr._1 + 1, x))
+        else curr :: encodeDirAux(xs, (1, x))
+    }
+    list match {
+      case Nil => Nil
+      case x :: xs => encodeDirAux(xs, (1, x))
+    }
+  }
+
+  def duplicate[T](list: List[T]): List[T] = list match {
+    case Nil => Nil
+    case x :: xs => x :: x :: duplicate(xs)
+  }
+
+  def duplicateN[T](n: Int, list: List[T]): List[T] = {
+    def duplicateAux(left: Int, list: List[T]): List[T] = list match {
+      case Nil => Nil
+      case x :: xs =>
+        if (left == 0) duplicateAux(n, xs)
+        else x :: duplicateAux(left - 1, x :: xs)
+    }
+    duplicateAux(n, list)
+  }
+
+  def drop[T](n: Int, list: List[T]): List[T] = ???
+
+  def split[T](n: Int, list: List[T]): (List[T], List[T]) = ???
+
+  def slice[T](i: Int, j: Int, list: List[T]): List[T] = ???
+
+  def rotate[T](n: Int, list: List[T]): List[T] = ???
+
+  def removeAt[T](i: Int, list: List[T]): (List[T], T) = ???
+
+  def insertAt[T](t: T, i: Int, list: List[T]): List[T] = ???
+
+  def range[T](i: Int, j: Int): List[Int] = ???
+
+  def randomSelect[T](n: Int, list: List[T]): List[T] = ???
+
+  def lotto[T](i: Int, j: Int): List[Int] = ???
+
+  def randomPermute[T](list: List[T]): List[T] = ???
+
+  def combinations[T](n: Int, list: List[T]): List[List[T]] = ???
+
+  def group3[T](list: List[T]): List[List[List[T]]] = ???
+
+  def groups[T](ns: List[Int], list: List[T]): List[List[List[T]]] = ???
+
+  def lsort[T](list: List[List[T]]): List[List[T]] = ???
+
+  def lsortFreq[T](list: List[List[T]]): List[List[T]] = ???
 }
 
